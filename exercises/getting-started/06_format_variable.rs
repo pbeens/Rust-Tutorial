7cc602a200a86f1609e1ac59ef082edc589004bf
Peter Beens <pbeens@gmail.com>
2025-05-19 22:01:49 -0400

    Create 06_format_variable.rs

---
+++ exercises/getting-started/06_format_variable.rs
@@ -0,0 +1,12 @@
+// 06_format_variable.rs
+// Use `format!` to create a "Welcome, {name}!" message and store it in a variable.
+// Then print that variable using `println!`.
+//
+// ▶️ Run in Playground:
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2024&gist=b8ca149db3033e1d60008e4f14470815
+
+fn main() {
+    let name = "Anika";
+    let greeting = format!();
+    println!("{}");
+}
